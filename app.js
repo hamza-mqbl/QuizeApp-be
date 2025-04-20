81fd7569f434137dcf6d32866ce1934b79097894
hamza-mqbl <hamzaaliics@gmail.com>
2025-04-20 07:49:17 -0700

    changed the url of frontend to correct one

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
   "http://localhost:3000", // Local development
   "http://localhost:8000", // Local development
   "http://13.60.95.182:8000",
-  "https://quiz-app-three-plum.vercel.app/",
+  "https://quiz-app-three-plum.vercel.app",
 ];
 // Middleware
 app.use(
