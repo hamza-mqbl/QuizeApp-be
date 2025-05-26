84e4c29345c0b584915f17db0f978280f1f94fd5
hamza-mqbl <hamzaaliics@gmail.com>
2025-05-26 03:38:00 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
 const express = require("express");
+const serverless = require("serverless-http");
+const connectDatabase = require("./db/Database");
 const quizRoutes = require("./routes/quizRoutes");
 const resultRoutes = require("./routes/resultRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const teacherRoutes = require("./routes/teacherRoutes");
-const serverless = require("serverless-http");
-const connectDatabase = require("./db/Database");
-const ErrorHandler = require("./middleware/error");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// Connect DB
+connectDatabase();
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-const bodyParser = require("body-parser");
-const cors = require("cors");
 const allowedOrigins = [
-  "http://localhost:3000", // Local development
-  "http://localhost:8000", // Local development
+  "http://localhost:3000",
+  "http://localhost:8000",
   "http://13.60.95.182:8000",
   "https://quiz-app-three-plum.vercel.app",
-  "https://quiz-app-zdp4.vercel.app"
+  "https://quiz-app-zdp4.vercel.app",
 ];
-// Middleware
+
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
@@ -34,25 +40,18 @@ app.use(
     credentials: true,
   })
 );
-app.use("/", express.static("uploads")); //setup done for 2nd branch
-// Increase the payload size limit
-app.use(bodyParser.json({ limit: "50mb" })); // Increase limit as needed
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
-// config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({
-    path: "backend/config/.env",
-  });
-}
+// Optional health check
 app.get("/is", (req, res) => {
   res.send("Server is running!");
 });
 
-// Routes
+// API routes
 app.use("/api/quiz", quizRoutes);
 app.use("/api/result", resultRoutes);
 app.use("/api/auth", studentRoutes);
 app.use("/api/teacher", teacherRoutes);
-module.exports = app;
-module.exports.handler = serverless(app);
+
+// Export for Vercel
+const handler = serverless(app);
+module.exports = { handler };
